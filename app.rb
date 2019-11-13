3f8fcaf7686af0e7812d9ac524c666d46628a1e1
Victoria Hale <vic.hale74@gmail.com>
2019-11-13 21:49:53 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -4,4 +4,10 @@ class App < Sinatra::Base
   get '/name' do 
     "My name is _"
   end
+  get '/hometown' do 
+    "My hometown is _"
+  end
+   get '/favorite-song' do 
+    "My favorite song is _"
+  end
 end
